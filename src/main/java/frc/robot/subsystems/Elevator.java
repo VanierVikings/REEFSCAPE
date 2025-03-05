65e2a76cc08e1a54155c63436595953418b23824
Ben <benxinboliang@gmail.com>
2025-03-05 18:48:50 -0500

    removed useless stuff

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,22 +1,18 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.ctre.phoenix6.configs.FeedbackConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.MotionMagicExpoVoltage;
 import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLimitSwitch;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.PersistMode;
@@ -35,7 +31,6 @@ public class Elevator extends SubsystemBase {
   private final TalonFX pivotMotorTwo;
   private final MotionMagicExpoVoltage m_request;
   private final TalonFXConfiguration pivotConfig;
-  private final FeedbackConfigs feedbackconfig;
   final MotionMagicVelocityVoltage pivotRequest = new MotionMagicVelocityVoltage(0);
   private final TrapezoidProfile.Constraints elevatorConstraints = new TrapezoidProfile.Constraints(
       ElevatorConstants.MAX_VELOCITY, ElevatorConstants.MAX_ACCELERATION);
@@ -43,19 +38,13 @@ public class Elevator extends SubsystemBase {
       ElevatorConstants.kI, ElevatorConstants.kD, elevatorConstraints, ElevatorConstants.kDt);
   private final ElevatorFeedforward elevatorFeedforward = new ElevatorFeedforward(ElevatorConstants.kS,
       ElevatorConstants.kG, ElevatorConstants.kV);
-      
-  private final TrapezoidProfile.Constraints pivotConstraints = new TrapezoidProfile.Constraints(
-      PivotConstants.MAX_VELOCITY, PivotConstants.MAX_ACCELERATION);
-  private final ArmFeedforward pivotFeedforward = new ArmFeedforward(PivotConstants.kS, PivotConstants.kG,
-      PivotConstants.kV);
+
 
   private final RelativeEncoder elevatorEncoder;
-  private final DutyCycleEncoder pivotOffsetEncoder = new DutyCycleEncoder(0, 360, 0);
   private final SparkMaxConfig elevatorMotorConfig;
   private final SparkLimitSwitch elevatorLimitSwitch;
 
   private final IdleMode elevatorIdleMode = IdleMode.kBrake;
-  private final IdleMode pivotIdleMode = IdleMode.kBrake;
 
   public enum Setpoint {
     kRest,
@@ -81,7 +70,6 @@ public class Elevator extends SubsystemBase {
     pivotConfig.Feedback.SensorToMechanismRatio = PivotConstants.positionConversionFactor;
     var slot0Configs = pivotConfig.Slot0;
 
-   feedbackconfig = new FeedbackConfigs();   
 
     slot0Configs.kS = PivotConstants.kS;
     slot0Configs.kV = PivotConstants.kV;
@@ -93,7 +81,6 @@ public class Elevator extends SubsystemBase {
     
     var motionMagicConfigs = pivotConfig.MotionMagic;
 
-    TalonFXConfiguration pivotFollow = new TalonFXConfiguration();
 
     motionMagicConfigs.MotionMagicCruiseVelocity = PivotConstants.MAX_VELOCITY; // Target cruise velocity of 80 rps
     motionMagicConfigs.MotionMagicAcceleration = PivotConstants.MAX_ACCELERATION; // Target acceleration of 160 rps/s (0.5 seconds)
